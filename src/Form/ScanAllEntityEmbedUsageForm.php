533bfcb71d0e70c7378fe5f47761482e6811b46a
Junaid PV <junaid@detlus.com>
2019-09-12 19:40:57 +0530

    Create page helping to run batch process scanning for all entity embeds.

---
+++ src/Form/ScanAllEntityEmbedUsageForm.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\entity_embed_usage\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Class ScanAllEntityEmbedUsageForm.
+ */
+class ScanAllEntityEmbedUsageForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'scan_all_entity_embed_usage_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['help'] = [
+      '#type' => 'item',
+      '#markup' => '<p>This page helps to scan all entity embeds in various content entities. The usage data will be store in database table.</p>',
+    ];
+
+    $form['actions'] = ['#type' => 'actions'];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => t('Scan for Embedded Entities'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $operations = [];
+
+    $entity_definitions =  \Drupal::entityTypeManager()->getDefinitions();
+
+    $supported_field_types = entity_embed_usage_supported_field_types();
+
+    foreach ($entity_definitions as $entity_type_id => $entity_type) {
+      if (!$entity_type->entityClassImplements(ContentEntityInterface::class) || !$entity_type->getBaseTable()) {
+        continue;
+      }
+      $bundles = \Drupal::service('entity_type.bundle.info')->getBundleInfo($entity_type_id);
+      foreach ($bundles as $bundle_name => $bundle_info) {
+        $fields = \Drupal::service('entity_field.manager')->getFieldDefinitions($entity_type_id, $bundle_name);
+        foreach ($fields as $field_info) {
+          $field_type = $field_info->getType();
+          if (in_array($field_type, array_keys($supported_field_types))) {
+            $operations[] = [
+              '\Drupal\entity_embed_usage\ScanAllEntityEmbedUsageBatch::scanForFieldInstance',
+              array($entity_type_id),
+            ];
+            break 2;
+          }
+        }
+      }
+    }
+
+    $batch = array(
+      'title' => t('Deleting Node...'),
+      'operations' => $operations,
+      'finished' => '\Drupal\entity_embed_usage\ScanAllEntityEmbedUsageBatch::finishBatchCallback',
+    );
+
+    batch_set($batch);
+  }
+}
---
+++ src/ScanAllEntityEmbedUsageBatch.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\entity_embed_usage;
+
+
+class ScanAllEntityEmbedUsageBatch {
+  public static function scanForFieldInstance($entity_type_id, &$context){
+    $message = t('Scanning for all entities of type @entity_type_id...', array('@entity_type_id' => $entity_type_id));
+    $results = array();
+
+    // Initiate multistep processing.
+    if (empty($context['sandbox'])) {
+      $context['sandbox']['progress'] = 0;
+      $context['sandbox']['max'] = \Drupal::entityQuery($entity_type_id)
+        ->count()
+        ->execute();
+    }
+
+    // Process the next 100 if there are at least 100 left. Otherwise,
+    // we process the remaining number.
+    $batch_size = 100;
+    $max = $context['sandbox']['progress'] + $batch_size;
+    if ($max > $context['sandbox']['max']) {
+      $max = $context['sandbox']['max'];
+    }
+
+    // Start where we left off last time.
+    $start = $context['sandbox']['progress'];
+
+    $query = \Drupal::entityQuery($entity_type_id)
+      ->range($context['sandbox']['progress'], $batch_size); //specify results to return
+    $entity_ids = $query->execute();
+
+    foreach ($entity_ids as $entity_id) {
+      $entity = \Drupal::entityTypeManager()->getStorage($entity_type_id)->load($entity_id);
+      entity_embed_usage_scan_for_embeds($entity);
+      // Update our progress!
+      $context['sandbox']['progress']++;
+    }
+
+    // Multistep processing : report progress.
+    if ($context['sandbox']['progress'] != $context['sandbox']['max']) {
+      $context['finished'] = $context['sandbox']['progress'] / $context['sandbox']['max'];
+    }
+    if ($context['finished']) {
+      drupal_set_message(t('Processed @count @entity_type_id items', ['@count' => $context['sandbox']['max'], '@entity_type_id' => $entity_type_id]));
+    }
+
+    $context['message'] = $message;
+    $context['results'] = $results;
+  }
+
+  function finishBatchCallback($success, $results, $operations) {
+    // The 'success' parameter means no fatal PHP errors were detected. All
+    // other error management should be handled using 'results'.
+    if ($success) {
+      $message = \Drupal::translation()->formatPlural(
+        count($results),
+        'One post processed.', '@count posts processed.'
+      );
+    }
+    else {
+      $message = t('Finished with an error.');
+    }
+    drupal_set_message($message);
+  }
+}
